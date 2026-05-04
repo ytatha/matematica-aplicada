7b338cea852c820a85d7c55033d092ca55896a8c
ytatha <agatha.nunes.souza@escola.pr.gov.br>
2026-05-04 07:39:17 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,79 @@
+const titulo = document.getElementById("titulo");
+
+const datas = [
+  {texto: "Passar no enem", data: "2026-11-11"},
+  {texto: "Aprender tailandês", data: "2026-07-25"},
+  {texto: "Passar na faculdade", data: "2027-02-01"},
+  {texto: "Entrar na área de trabalho", data: "2027-02-05"}
+];
+
+let atual = 0;
+
+// trocar abas
+document.querySelectorAll(".tab").forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    document.querySelectorAll(".tab").forEach(b => b.classList.remove("active"));
+    btn.classList.add("active");
+
+    atual = i;
+    titulo.innerText = datas[i].texto;
+  });
+});
+
+// timer
+function atualizarTempo() {
+  const destino = new Date(datas[atual].data).getTime();
+  const agora = new Date().getTime();
+  const diff = destino - agora;
+
+  const dias = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const horas = Math.floor((diff / (1000 * 60 * 60)) % 24);
+  const min = Math.floor((diff / (1000 * 60)) % 60);
+  const seg = Math.floor((diff / 1000) % 60);
+
+  document.getElementById("dias").innerText = dias;
+  document.getElementById("horas").innerText = horas;
+  document.getElementById("min").innerText = min;
+  document.getElementById("seg").innerText = seg;
+}
+
+Postada por DANIELA DE JESUS MEDINA MARTINEZ
+DANIELA DE JESUS MEDINA MARTINEZ
+Criado em: 07:3607:36
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Objetivos</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="container">
+  <h2>Meus objetivos do ano_</h2>
+
+  <div class="tabs">
+    <button class="tab active" data-target="1">Passar no enem</button>
+    <button class="tab" data-target="2">Aprender tailandês</button>
+    <button class="tab" data-target="3">Passar na faculdade</button>
+    <button class="tab" data-target="4">Entrar na área de trabalho</button>
+  </div>
+
+  <div class="card">
+    <h1 id="titulo">COMPLETAR MEUS OBJETIVOS</h1>
+
+    <p class="sub">TEMPO PARA COMPLETAR OBJETIVO</p>
+
+    <div class="timer">
+      <div><span id="dias">0</span><small>dias</small></div>
+      <div><span id="horas">0</span><small>horas</small></div>
+      <div><span id="min">0</span><small>min</small></div>
+      <div><span id="seg">0</span><small>seg</small></div>
+    </div>
+  </div>
+</div>
+
+<script src="main.js"></script>
+</body>
+</html>
 
